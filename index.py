c099ad4f44080aa625f0c74d7182746ce3b06e64
PaRebin <65243148+parebin@users.noreply.github.com>
2020-12-01 14:07:30 +0100

    Update index.py

--- index.py
+++ index.py
@@ -19,3 +19,19 @@ film = st.selectbox('select_film', liste_film, 2)
 
 st.subheader('film choisi')
 st.write(film)
+
+_radio_button = components.declare_component(
+    "radio_button", url="http://localhost:3001",
+)
+
+
+def custom_radio_button(label, options, default, key=None):
+    return _radio_button(label=label, options=options, default=default, key=key)
+
+
+result = custom_radio_button(
+    "How many bats?",
+    options=["one bat", "TWO bats", "THREE bats", "FOUR BATS! ah ah ah!"],
+    default="one bat",
+)
+st.write("This many: %s" % result)
