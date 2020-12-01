20ea4aea9720fd8439b2d680af8afbc51401ff5b
PaRebin <65243148+parebin@users.noreply.github.com>
2020-12-01 09:50:01 +0100

    Update index.py

--- index.py
+++ index.py
@@ -12,3 +12,14 @@ film = st.selectbox('select_film', liste_film, 1)
 
 st.subheader('film choisi')
 st.write(film)
+
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://www.imdb.com/title/tt0120338/"
+
+pageweb = requests.get(url)
+
+scrap = BeautifulSoup(pageweb.text, "html.parser")
+
+st.image(scrap.find_all("div", attrs="poster") [0].img)
