a269d4a8feb837178ad64263c7219dfeb2ea18ec
PaRebin <65243148+parebin@users.noreply.github.com>
2020-12-01 09:23:25 +0100

    Update index.py

--- index.py
+++ index.py
@@ -6,4 +6,4 @@ import time
 import streamlit.components.v1 as components
 
 # embed streamlit docs in a streamlit app
-components.iframe("https://docs.streamlit.io/en/latest")
+components.iframe("https://www.imdb.com/title/tt0120338/?ref_=nv_sr_srsg_0")
