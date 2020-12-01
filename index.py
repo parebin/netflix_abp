a423f0ae46299540859ad71251595ce2ca4c65ce
PaRebin <65243148+parebin@users.noreply.github.com>
2020-12-01 09:27:37 +0100

    Update index.py

--- index.py
+++ index.py
@@ -7,3 +7,6 @@ import streamlit.components.v1 as components
 
 # embed streamlit docs in a streamlit app
 components.iframe("https://lechaudron.io/tag/nantes/")
+
+
+st.sidebar.slider("Choose a frame (index)", 0, len(selected_frames) - 1, 0)
