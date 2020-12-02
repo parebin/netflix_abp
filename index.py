dbf94511a134f73a0d7364362409b98c92898c3e
parebin <pa.rebin@gmail.com>
2020-12-02 15:35:15 +0100

    modif biblio 2

--- index.py
+++ index.py
@@ -3,6 +3,8 @@ import numpy as np
 import random
 import streamlit as st
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import NearestNeighbors
 import streamlit.components.v1 as components
 # from sklearn.preprocessing import StandardScaler
 # from sklearn.neighbors import NearestNeighbors
