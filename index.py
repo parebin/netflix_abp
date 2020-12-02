c606f7271f8751373a87d6fb3fee8fabbe7fd7a3
parebin <pa.rebin@gmail.com>
2020-12-02 15:56:54 +0100

    import

--- index.py
+++ index.py
@@ -2,9 +2,10 @@ import pandas as pd
 import numpy as np
 import random
 import streamlit as st
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+import sklearn
 from sklearn.neighbors import NearestNeighbors
-from sklearn.preprocessing import StandardScaler
+# from sklearn.preprocessing import StandardScaler
 import streamlit.components.v1 as components
 # from sklearn.preprocessing import StandardScaler
 # from sklearn.neighbors import NearestNeighbors
