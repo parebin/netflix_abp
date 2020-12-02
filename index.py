0d31bf179e36d5eccd2deb43c4ab8b64ef8d3ecb
parebin <pa.rebin@gmail.com>
2020-12-02 10:42:45 +0100

    modif img

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ import time
 import streamlit.components.v1 as components
 
 #url = "https://image.tmdb.org/t/p/w500/wwemzKWzjKYJFfCeiB57q3r4Bcm.png"
-url = "image.png"
+url = "https://github.com/parebin/netflix_abp/blob/main/image.png"
 
 # embed streamlit docs in a streamlit app
 components.iframe(url)
