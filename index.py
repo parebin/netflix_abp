61796c03f16601cb00c2613ee074cc744fab07e8
parebin <pa.rebin@gmail.com>
2020-12-02 16:11:29 +0100

    a

--- index.py
+++ index.py
@@ -1,14 +1,13 @@
+
+import sklearn
 import pandas as pd
 import numpy as np
 import random
 import streamlit as st
-# import matplotlib.pyplot as plt
-import scikit-learn as sklearn
 from sklearn.neighbors import NearestNeighbors
-# from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler
 import streamlit.components.v1 as components
-# from sklearn.preprocessing import StandardScaler
-# from sklearn.neighbors import NearestNeighbors
+
 
 
 # image Netflix
