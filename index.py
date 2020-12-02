73093d90a0a61fcb405e553d74067f8cbcbab0d1
parebin <pa.rebin@gmail.com>
2020-12-02 10:41:37 +0100

    insert image

--- index.py
+++ index.py
@@ -5,7 +5,8 @@ import streamlit as st
 import time
 import streamlit.components.v1 as components
 
-url = "https://image.tmdb.org/t/p/w500/wwemzKWzjKYJFfCeiB57q3r4Bcm.png"
+#url = "https://image.tmdb.org/t/p/w500/wwemzKWzjKYJFfCeiB57q3r4Bcm.png"
+url = "image.png"
 
 # embed streamlit docs in a streamlit app
 components.iframe(url)
